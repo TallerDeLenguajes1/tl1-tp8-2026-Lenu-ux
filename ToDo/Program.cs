ca51dad768025ce3a46081931044d76c280cc771
Lenis <lenis99lg@gmail.com>
2026-06-09 06:40:48 -0300

    subir carpetas

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -3,5 +3,8 @@
 //creo las listas de tareas realizadas y pendientes
 List<Tarea> tareasPendientes= new List<Tarea> ();
 List<Tarea> tareasRealizadas= new List<Tarea> ();
+//2.cargo n tareas
 
+Random aleatorio=new Random();
+System.Console.WriteLine("Ingrese la cantidad de tareas a crear: ");
 
