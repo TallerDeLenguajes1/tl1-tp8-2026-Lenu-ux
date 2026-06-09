8501393c91ebda7ad09f24b273e113ba0b5c29b4
Lenis <lenis99lg@gmail.com>
2026-06-08 21:34:46 -0300

    correccion de verificar la duracion de la clase

--- ToDo/Tarea.cs
+++ ToDo/Tarea.cs
@@ -1,17 +1,29 @@
 namespace TareaApp;
 public class Tarea
 {
-  public int TareaID { get; set; }
+public int TareaID { get; set; }
  public string Descripcion { get; set; }
- public int Duracion { get; set; }
 //debo validar que duracion este entre 10 y 100 fuera del constructor
-          
+  private int duracionReal;
+ public int Duracion { get {return duracionReal;}
+        set
+        {
+            if (value >=10 && value<=100)
+            {
+                duracionReal=value;
+            }else
+            {
+                duracionReal=10;
+            }
+        }
+         }
+
 //armo un constructor
 public Tarea(int id,string descripcion,int duracion)
     {
         this.TareaID=id;
         this.Descripcion=descripcion;
-    
+        this.Duracion=duracion;
     }
 
 }
