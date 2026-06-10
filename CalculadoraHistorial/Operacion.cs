d75093cee78c490522908a80458db1fc45791876
Lenis <lenis99lg@gmail.com>
2026-06-09 21:53:53 -0300

    iniciando ejercicio 2

---
+++ CalculadoraHistorial/Operacion.cs
@@ -0,0 +1,11 @@
+
+public class Operacion
+{
+    private double resultadoAnterior;
+    private double nuevoValor;
+    private TipoOperacion operacion;
+    public double Resltado
+    {
+        
+    }
+}
